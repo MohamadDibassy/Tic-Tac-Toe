49618953bbc0fd4f46c158b21c2d1c180d939a45
MohamadDibassy <mbdibass@buffalo.edu>
2021-06-22 19:12:51 -0400

    Recreate dom nodes on the fly
    Each time drawboard is called delete existing dom nodes and recreate everything from scratch.
    Reduce amount of global variables

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
-const boxes = Array.from(document.getElementsByClassName("box")); //manipulate the DOM
-const playText = document.getElementById("playText"); //manipulate the DOM
-const restartBtn = document.getElementById("restartBtn"); //manipulate the DOM
-const header__turn = document.getElementById("header__turn"); //manipulate the DOM
-
 const spaces = [];
+const rootDomNode = document.body;
+
 const O_TEXT = "0";
 const X_TEXT = "X";
-let currentPlayer = O_TEXT;
-
-b0 = document.getElementById("0"); //manipulate the DOM
-b1 = document.getElementById("1"); //manipulate the DOM
-b2 = document.getElementById("2"); //manipulate the DOM
-b3 = document.getElementById("3"); //manipulate the DOM
-b4 = document.getElementById("4"); //manipulate the DOM
-b5 = document.getElementById("5"); //manipulate the DOM
-b6 = document.getElementById("6"); //manipulate the DOM
-b7 = document.getElementById("7"); //manipulate the DOM
-b8 = document.getElementById("8"); //manipulate the DOM
 
 function selectwinnerBoxes(box1, box2, box3) {
   //manipulate the DOM
@@ -25,8 +11,45 @@ function selectwinnerBoxes(box1, box2, box3) {
   box3.classList.add("board__tile--winner");
 }
 
-const drawBoard = () => {
-  //manipulate the DOM
+// In fact, each time you call drawBoard, I would delete all of the existing DOM nodes
+// and re-create everything from scratch.
+// This isn’t very efficient, but for a small program like this, it actually works very well.
+
+const drawBoard = (rootDomNode, spaces) => {
+  rootDomNode.innerHTML = `
+    <div class="container">
+      <h1 id="playText">Let's Play</h1>
+      <h2 id="header__turn"></h2>
+      <button id="restartBtn">Restart</button>
+      <div id="gameboard">
+        <div class="box" id="0"></div>
+        <div class="box" id="1"></div>
+        <div class="box" id="2"></div>
+        <div class="box" id="3"></div>
+        <div class="box" id="4"></div>
+        <div class="box" id="5"></div>
+        <div class="box" id="6"></div>
+        <div class="box" id="7"></div>
+        <div class="box" id="8"></div>
+      </div>
+    </div>
+    `;
+
+  const boxes = Array.from(document.getElementsByClassName("box")); //manipulate the DOM
+  const playText = document.getElementById("playText"); //manipulate the DOM
+  const restartBtn = document.getElementById("restartBtn"); //manipulate the DOM
+  const header__turn = document.getElementById("header__turn"); //manipulate the DOM
+
+  b0 = document.getElementById("0"); //manipulate the DOM
+  b1 = document.getElementById("1"); //manipulate the DOM
+  b2 = document.getElementById("2"); //manipulate the DOM
+  b3 = document.getElementById("3"); //manipulate the DOM
+  b4 = document.getElementById("4"); //manipulate the DOM
+  b5 = document.getElementById("5"); //manipulate the DOM
+  b6 = document.getElementById("6"); //manipulate the DOM
+  b7 = document.getElementById("7"); //manipulate the DOM
+  b8 = document.getElementById("8"); //manipulate the DOM
+
   boxes.forEach((box, index) => {
     let stylestring = "";
     if (index < 3) {
@@ -44,12 +67,15 @@ const drawBoard = () => {
     box.style = stylestring;
     box.addEventListener("click", boxClicked);
   });
+
+  restartBtn.addEventListener("click", restart);
 };
 
 const boxClicked = e => {
   //mutate state of application
   const id = e.target.id;
   if (!spaces[id]) {
+    const currentPlayer = currentPlayerFromState(spaces);
     spaces[id] = currentPlayer;
     e.target.innerText = currentPlayer;
     header__turn.innerHTML = `${currentPlayer}'s turn`;
@@ -58,11 +84,23 @@ const boxClicked = e => {
       playText.innerHTML = `${currentPlayer} has won!`;
       return;
     }
-    currentPlayer = currentPlayer === O_TEXT ? X_TEXT : O_TEXT;
   }
 };
+
+// // This function calculates the current player from `boxes`. Since we know that
+// // players will alternate taking moves, we can deduce who the current player is
+// // from the number of "X"s and "O"s in `boxes`.
+
+function currentPlayerFromState(spaces) {
+  const numOs = spaces.filter(b => b === O_TEXT).length;
+  const numXs = spaces.filter(b => b === X_TEXT).length;
+  return numXs < numOs ? X_TEXT : O_TEXT;
+}
+
 const playerHasWon = () => {
   //mutate state of application
+  const currentPlayer = currentPlayerFromState(spaces);
+
   if (
     spaces[2] == currentPlayer &&
     spaces[4] == currentPlayer &&
@@ -115,23 +153,18 @@ const playerHasWon = () => {
   }
 };
 
-//mutate state of application
 const restart = () => {
   spaces.forEach((space, index) => {
     spaces[index] = null;
   });
-  boxes.forEach(box => {
-    box.innerHTML = "";
-    box.classList.remove("board__tile--winner");
-  });
-  playText.innerHTML = `Let's Play!`;
-  currentPlayer = O_TEXT;
+  drawBoard(rootDomNode, spaces);
 };
 
-restartBtn.addEventListener("click", restart);
-drawBoard();
+//mutate state of application
+
+drawBoard(rootDomNode, spaces);
 
 //const mapStateToProps = (state = state => ({
-  //spaces: state.spaces
+//spaces: state.spaces
 //}));
 //export default connect(mapStateToProps)();
